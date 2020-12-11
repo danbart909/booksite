21a729821e527c49bfd6da41330c6effcd2f1792
Dan Bartlett <danbart909@gmail.com>
2020-12-10 20:21:08 -0500

    update about the author and some fonts

--- src/components/AboutTheAuthor.js
+++ src/components/AboutTheAuthor.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import frontdown from '../images/frontdown.jpg'
 
 export default class AboutTheAuthor extends Component {
   render() {
     return (
       <div id='abouttheauthor-container'>
-        <span>About the Author</span>
+        <div id='ata-1'>
+          <div id='ata-title'>
+            <span>About the Author</span>
+          </div>
+        </div>
+        <div id='ata-2'>
+          <div id='ata-textbody'>
+            <div id='ata-text-1'>
+              <span>Marc Twaine is a 15 year veteran of the Human Services, Psychotherapy, Relationship Therapy, and Life Coach professions. He has a Masters Degree and Doctorate in related fields but the true value of his words rest in his personal brand of views, introspect, and wisdom around friendship development, dating dynamics, relationships, and courtship. His talent for sharing his life experiences in a way that integrate common issues that men and women face in life and dating demonstrate how we as individuals face more or less the same group of challenges in seeking connections. scope of insight that bring refreshing, tell it exactly how it is insight to topics... In this book, he pulls on his own missteps and lessons learned from being a womanizer to give women a tutorial on how to interact with, love, and understand men that can be described as Man-Whores.</span>
+            </div>
+            <div id='ata-text-2'>
+              <span>Born Again Man-Whore is an exploration of male sexual development and the psychosocial circumstances that typically turn men into manwhores, whether desire for multiple women is a biological function and organic by product of procreation programming in males or a behavior that falls more into the conscious choice born out of pleasure seeking category. Does men decieving women into sexual interactions via lies and half truths constitute robbing a women of the ability to give truly informed consent constitutes non-consentual sex.</span>
+            </div>
+          </div>
+          <div id='ata-authorpic'>
+            {/* <img src={frontdown} alt='the author'></img> */}
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/WhereToBuy.js
+++ src/components/WhereToBuy.js
@@ -6,7 +6,37 @@ export default class WhereToBuy extends Component {
   render() {
     return (
       <div id='wheretobuy-container'>
-        <span>Where to Buy</span>
+        {/* <div id='wtb-box'>
+          <div id='wtb-title'>
+            <span>Purchase from:</span>
+          </div>
+          <div id='wtb-retailers'>
+            <div id='wtb-amazon'>
+              <div id='wtb-amazon-left'>
+                <span>Buy from Amazon</span>
+              </div>
+              <div id='wtb-amazon-right'>
+                <span>*img*</span>
+              </div>
+            </div>
+            <div id='wtb-bn'>
+              <div id='wtb-bn-left'>
+                <span>Buy from Barnes & Noble</span>
+              </div>
+              <div id='wtb-bn-right'>
+                <span>*img*</span>
+              </div>
+            </div>
+            <div id='wtb-anything'>
+              <div id='wtb-anything-left'>
+                <span>Buy from The Black Web</span>
+              </div>
+              <div id='wtb-anything-right'>
+                <span>*img*</span>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
     )
   }
